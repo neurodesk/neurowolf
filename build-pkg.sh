7a2bddf637179ee2f0ef176ca03990248113ae73
Emily Trau <emily@downunderctf.com>
2025-10-22 14:47:21 +1100

    tree-wide: make recipes compatible with wolfi/os

--- build-pkg.sh
+++ build-pkg.sh
@@ -3,4 +3,13 @@ set -ex
 PKG="$1"
 docker run --privileged --rm -v "${PWD}":/work \
   cgr.dev/chainguard/melange build pkgs/${PKG}/melange.yaml \
-  --signing-key melange.rsa 
+  --arch x86_64,aarch64 \
+  --namespace neurowolf \
+  --license 'Apache-2.0' \
+  --git-repo-url 'https://github.com/neurodesk/neurowolf' \
+  --repository-append ./packages \
+  --keyring-append ./melange.rsa.pub \
+  --repository-append "https://packages.wolfi.dev/os" \
+  --keyring-append "https://packages.wolfi.dev/os/wolfi-signing.rsa.pub" \
+  --pipeline-dir ./extern/pipelines \
+  --signing-key melange.rsa
--- test-pkg.sh
+++ test-pkg.sh
@@ -3,7 +3,10 @@ set -ex
 PKG="$1"
 docker run --privileged --rm -v "${PWD}":/work \
   cgr.dev/chainguard/melange test pkgs/${PKG}/melange.yaml ${PKG} \
-  --repository-append /work/packages \
-  --keyring-append /work/melange.rsa.pub \
-  --repository-append https://packages.wolfi.dev/os \
-  --keyring-append https://packages.wolfi.dev/os/wolfi-signing.rsa.pub
+  --arch x86_64,aarch64 \
+  --repository-append ./packages \
+  --keyring-append ./melange.rsa.pub \
+  --repository-append "https://packages.wolfi.dev/os" \
+  --keyring-append "https://packages.wolfi.dev/os/wolfi-signing.rsa.pub" \
+  --pipeline-dirs ./extern/pipelines \
+  --test-package-append wolfi-base
